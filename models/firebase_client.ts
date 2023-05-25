e6f590dadc98e43aa208e90df2a94405328094c2
nappy <zhffktkdlekghksxk@naver.com>
2023-05-25 17:26:39 +0900

    Feat:"firebase_Client 코드 추가"

---
+++ models/firebase_client.ts
@@ -0,0 +1,35 @@
+import { getApps, initializeApp } from 'firebase/app';
+import { Auth, getAuth } from 'firebase/auth';
+
+const FirebaseCredentials = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+};
+
+export default class FirebaseClient {
+  private static instance: FirebaseClient;
+
+  private auth: Auth;
+
+  public constructor() {
+    const apps = getApps();
+    if (apps.length === 0) {
+      console.info('firebase client init start');
+      initializeApp(FirebaseCredentials);
+    }
+    this.auth = getAuth();
+    console.info('firebase auth');
+  }
+
+  public static getInstance(): FirebaseClient {
+    if (FirebaseClient.instance === undefined || FirebaseClient.instance === null) {
+      FirebaseClient.instance = new FirebaseClient();
+    }
+    return FirebaseClient.instance;
+  }
+
+  public get Auth(): Auth {
+    return this.auth;
+  }
+}
