fbb95130d893a827e9e1d94cc4325b77515aaef7
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2023-05-26 17:36:30 +0900

    Feat:"GNB 작업 및 UI 수정"

--- components/google_login_button.tsx
+++ components/google_login_button.tsx
@@ -2,23 +2,23 @@ import { Box, Button } from '@chakra-ui/react';
 import { on } from 'events';
 
 interface Props {
-  onClick:() => void;
+  onClickLogin:() => void;
 }
 
-export const GoogleLoginButton = function ({onClick}) {
+export const GoogleLoginButton = function ({onClickLogin}:Props) {
   return (
     <Box>
       <Button
         size="lg"
         width="full"
-        margin="6"
-        borderRadius="20px"
+        // margin="6"
+        borderRadius="full"
         bgColor="#4285f4"
         color="white"
         colorScheme="blue"
-        leftIcon={<img src="/google.svg" alt="google 로고" />}
+        leftIcon={<img src="/google.svg" alt="google 로고" style={{ backgroundColor: 'white', padding: '8px' }} />}
         border="none"
-        onClick={onClick}
+        onClick={onClickLogin}
       >
         Google 계정으로 시작하기
       </Button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { NextPage } from 'next';
 import { Flex, Box, Heading, Center } from '@chakra-ui/react';
 import { ServiceLayout } from '@/components/service_layout';
 import { GoogleLoginButton } from '@/components/google_login_button';
@@ -7,18 +6,18 @@ import { useAuth } from '@/contexts/auth_user.context';
 
 
 const IndexPage: NextPage = function () {
-const {signInWithGoogle} = useAuth();
+  const { signInWithGoogle } = useAuth();
   return (
     <ServiceLayout title="test" height="100vh" backgroundColor="gray.50">
-      <Box maxW="md" mx="auto">
-        <Center marginTop="20" marginBottom="10" p="6">
-          <img src="/main_logo.svg" alt="메인 로고" />
-          <Flex justify="center">
-            <Heading>#BlahBlah</Heading>
-          </Flex>
-        </Center>
+      <Box maxW="md" mx="auto" pt='10'>
+        <img src="/main_logo.svg" alt="메인 로고" />
+        <Flex justify="center">
+          <Heading>#BlahBlah</Heading>
+        </Flex>
       </Box>
-      <GoogleLoginButton onClick={signInWithGoogle} />
+      <Center marginTop="20">
+        <GoogleLoginButton onClick={signInWithGoogle} />
+      </Center>
     </ServiceLayout>
   );
 };
